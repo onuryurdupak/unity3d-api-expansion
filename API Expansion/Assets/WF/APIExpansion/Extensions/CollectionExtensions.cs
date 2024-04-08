0d50b7d7eb0d50c72515aebae1b7a852c5131230
Onur YURDUPAK <onuryurdupak@gmail.com>
2024-04-08 18:51:12 +0300

    Add array 'Find' extensions

--- API Expansion/Assets/WF/APIExpansion/Extensions/CollectionExtensions.cs
+++ API Expansion/Assets/WF/APIExpansion/Extensions/CollectionExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public static class CollectionExtensions
 {
-    private static Random random = new Random();
+    private static readonly Random random = new();
 
     public static void Shuffle<T>(this T[] array)
     {
@@ -27,5 +27,29 @@ public static class CollectionExtensions
             list[i] = list[j];
             list[j] = temp;
         }
+    }
+
+    public static T Find<T>(this T[] array, Predicate<T> match)
+    {
+        foreach (T item in array)
+        {
+            if (match(item))
+                return item;
+        }
+
+        return default;
+    }
+
+    public static T[] FindAll<T>(this T[] array, Predicate<T> match)
+    {
+        List<T> result = new();
+
+        foreach (T item in array)
+        {
+            if (match(item))
+                result.Add(item);
+        }
+
+        return result.ToArray();
     }
 }
--- API Expansion/Assets/WF/APIExpansion/Metadata.cs
+++ API Expansion/Assets/WF/APIExpansion/Metadata.cs
@@ -2,6 +2,6 @@
 {
     public static class Metedata
     {
-        public const string Version = "0.3.0";
+        public const string Version = "0.3.1";
     }
 }
